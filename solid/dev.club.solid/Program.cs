3fc44fe80274619082983c057f801567244f0f59
Sergey Babitsky <SergeyBabitsky@coherentsolutions.com>
2022-01-19 09:25:47 +0200

    host update

--- solid/dev.club.solid/Program.cs
+++ solid/dev.club.solid/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using dev.club.solid.wcf;
+
+var validator = new KeyVaultCertificateValidator(null!, null!);
+validator.Validate(certificate: null!);
 Console.WriteLine("Hello, World!");
