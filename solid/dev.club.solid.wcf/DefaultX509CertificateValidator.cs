dcb59c18b5b0dc8cb5ae4e6282872d0d543bd74b
Sergey Babitsky <SergeyBabitsky@coherentsolutions.com>
2022-01-26 09:16:47 +0200

    Updates

--- solid/dev.club.solid.wcf/DefaultX509CertificateValidator.cs
+++ solid/dev.club.solid.wcf/DefaultX509CertificateValidator.cs
@@ -5,11 +5,15 @@ namespace dev.club.solid.wcf
 {
     internal class DefaultX509CertificateValidator : X509CertificateValidator
     {
+        public DefaultX509CertificateValidator(string trustType)
+        {
+            
+        }
         public override void Validate(X509Certificate2 certificate)
         {
             
         }
 
-        public static X509CertificateValidator PeerOrChainTrust { get; }
+        public static X509CertificateValidator PeerOrChainTrust => new DefaultX509CertificateValidator(nameof(PeerOrChainTrust));
     }
 }
